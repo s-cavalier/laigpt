5fa0d4ca39e4c42f63d0c174eafd59d42e7b8efe
s-cavalier <jrc974@ucsb.edu>
2025-12-02 21:47:17 -0800

    memory arena

--- cppautograd/src/Tensor.hpp
+++ cppautograd/src/Tensor.hpp
@@ -1,57 +1,57 @@
 #ifndef __TENSOR_HPP__
 #define __TENSOR_HPP__
 
-#include <xtensor/xarray.hpp>
-#include <vector>
 #include <cstddef>
-#include "PolyPtr.hpp"
 #include <memory>
 
 namespace ag {
 
-    using f32Array = xt::xarray<float>;
+    class TensorNetwork {
 
-    class GradientFunction {
+        class MemoryArena {
+            std::byte* buffer;
+            size_t size;
+            size_t offset;
 
-        virtual f32Array forward(const std::vector< f32Array >& inputs) = 0;
-        virtual std::vector<f32Array> backward( const f32Array& output, const std::vector<f32Array>& inputs ) = 0;
+        public:
+            explicit MemoryArena(size_t bytes);
+            ~MemoryArena() noexcept;
+
+            MemoryArena(const MemoryArena&) = delete;
+            MemoryArena& operator=(const MemoryArena&) = delete;
+
+            MemoryArena(MemoryArena&& other) = delete;
+            MemoryArena& operator=(MemoryArena&& other) = delete;
+
+            template <class T>
+            T* allocate(size_t n = 1) {
+                constexpr size_t alignment = alignof(T);
+                constexpr size_t bytes = n * sizeof(T);
+
+                void* ptr = (void*)(buffer + offset);
+                size_t space = size - offset;
+                
+                if ( !std::align( alignment, bytes, ptr, space ) ) throw std::bad_alloc("TensorNetwork ran out of memory. Try using an obj with more memory.");
+                
+                offset = ( (std::byte*)(ptr) - buffer ) + bytes;
+                return (T*)ptr;
+            }
+
+            void reset() noexcept { offset = 0; }
+
+
+        };
+
+
+
+
+
 
     };
 
-    class DiagonalJacobianGradient : public GradientFunction {
-        std::vector<f32Array> backward( const f32Array& output, const std::vector<f32Array>& inputs ) final override;
-    };
 
-    
-
-    f32Array reduce_to_shape( const f32Array& grad, const std::vector<size_t>& shape );
-
-
-    using GradFnPtr = PolyPtr<GradientFunction, 12>;
-
-    class Tensor {
-        std::unique_ptr<f32Array> values; // values must be on the heap because an f32Array is somehow 240 bytes each
-        std::vector< Tensor > parents; // Tensors should be moved into parents
-        GradFnPtr gradient;
-
-    public:
-        Tensor( bool track_gradient = true );
-        explicit Tensor( f32Array&& array, bool track_gradient = true ); // Take ownership
-        explicit Tensor( const f32Array& array, bool track_gradient = true ); // Copy
-
-        Tensor(const Tensor&) = delete;
-        Tensor& operator=(const Tensor&) = delete;
-
-        inline f32Array& data();
-        inline void addParent(Tensor&& t);
-        inline void setGradientFunction( PolyPtr<GradientFunction, 12>&& grad_fn );
-
-        void backpropagate();
-
-        float gradient_value;
-        bool track_gradient;
-    };
 
 }
+
 
 #endif
