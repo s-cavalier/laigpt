50537bce7a43201e2a764e65784ed52e7996baa3
s-cavalier <jrc974@ucsb.edu>
2025-12-02 17:46:32 -0800

    Utils

---
+++ cppautograd/src/PolyRef.hpp
@@ -0,0 +1,211 @@
+#ifndef __POLY_REF_HPP__
+#define __POLY_REF_HPP__
+
+#include <cstddef>
+#include <cstdint>
+#include <new>
+#include <type_traits>
+#include <cstring>
+
+/*
+Polymorphic Pointer class <br>
+It's intended as a slightly more runtime optimized unique ptr with the cost of a bit more memory, it can do:
+1. If a static reference exists, the pointer can store a pointer to that static variable without running delete on it. 
+This can prevent heap-allocs in the case of empty types, like Functors or callables derived from Base.
+2. If the derived type is small in size, it will store it inline via SBO. The size of the SBO buffer is templated.
+This helps prevent unnecessary heap allocs.
+3. If the derived type is too large for the SBO, it effectively just acts as a slightly fatter unique_ptr.
+
+
+*/
+template <class Base, size_t SBO_SIZE = 32>
+class PolyRef {
+public:
+    enum Mode : uint8_t { Unspecified, Static, SBO, Heap };
+
+private:
+    union Storage {
+        std::byte sbo[SBO_SIZE];
+        Base*     ptr;
+        Storage() {}
+        ~Storage() {}
+    } _storage{};
+
+    Mode _mode = Unspecified;
+
+    Base* raw_ptr() {
+        switch (_mode) {
+        case Static:
+        case Heap: return _storage.ptr;
+        case SBO:  return reinterpret_cast<Base*>(_storage.sbo);
+        default:   return nullptr;
+        }
+    }
+
+    const Base* raw_ptr() const {
+        switch (_mode) {
+        case Static:
+        case Heap: return _storage.ptr;
+        case SBO:  return reinterpret_cast<const Base*>(_storage.sbo);
+        default:   return nullptr;
+        }
+    }
+
+    void destroy() {
+        switch (_mode) {
+        case Static:
+            break;
+        case SBO:
+            if (auto* p = raw_ptr()) p->~Base();
+            break;
+        case Heap:
+            delete _storage.ptr;
+            _storage.ptr = nullptr;
+            break;
+        default:
+            break;
+        }
+        _mode = Unspecified;
+    }
+
+    void move_from(PolyRef&& other) {
+        _mode = other._mode;
+
+        switch (other._mode) {
+        case Static:
+            _storage.ptr = other._storage.ptr;
+            break;
+
+        case SBO:
+            std::memcpy(_storage.sbo, other._storage.sbo, SBO_SIZE);
+            if (auto* p = other.raw_ptr()) p->~Base();
+            break;
+
+        case Heap:
+            _storage.ptr = other._storage.ptr;
+            other._storage.ptr = nullptr;
+            break;
+
+        default:
+            break;
+        }
+
+        other._mode = Unspecified;
+    }
+
+public:
+    PolyRef() = default;
+
+    PolyRef(const PolyRef& other) {
+        _mode = other._mode;
+
+        switch (_mode) {
+        case Static:
+            _storage.ptr = other._storage.ptr;
+            break;
+
+        case SBO:
+            other.raw_ptr()->stack_clone(_storage.sbo);
+            break;
+
+        case Heap:
+            _storage.ptr = other.raw_ptr()->heap_clone();
+            break;
+
+        default:
+            _storage.ptr = nullptr;
+            break;
+        }
+    }
+
+    PolyRef& operator=(const PolyRef& other) {
+        if (this == &other) return *this;
+
+        destroy();
+        _mode = other._mode;
+
+        switch (_mode) {
+        case Static:
+            _storage.ptr = other._storage.ptr;
+            break;
+
+        case SBO:
+            other.raw_ptr()->stack_clone(_storage.sbo);
+            break;
+
+        case Heap:
+            _storage.ptr = other.raw_ptr()->heap_clone();
+            break;
+
+        default:
+            _storage.ptr = nullptr;
+            break;
+        }
+
+        return *this;
+    }
+
+    PolyRef(PolyRef&& other) noexcept {
+        move_from(std::move(other));
+    }
+
+    PolyRef& operator=(PolyRef&& other) noexcept {
+        if (this != &other) {
+            destroy();
+            move_from(std::move(other));
+        }
+        return *this;
+    }
+
+    ~PolyRef() {
+        destroy();
+    }
+
+
+    template<class T, class... Args>
+    static PolyRef emplace(Args&&... args) {
+        static_assert(std::is_base_of_v<Base, T>, "T must derive from Base");
+
+        PolyRef p;
+
+        constexpr bool is_static = sizeof(T) == sizeof(void*); // If it just has one vptr it'll be size at least 8, and if it onyl has the vptr, it's size 8. It must have a vptr since we statically assert it derives from Base.
+
+        constexpr bool fits_sbo = sizeof(T) <= SBO_SIZE && alignof(T) <= alignof(std::max_align_t);
+
+        if constexpr (is_static) {
+            static T singleton{};
+            p._mode = Static;
+            p._storage.ptr = &singleton;
+        }
+
+        else if constexpr (fits_sbo) {
+            void* buf = p._storage.sbo;
+            new (buf) T(std::forward<Args>(args)...);
+            p._mode = SBO;
+        }
+
+        else {
+            p._storage.ptr = new T(std::forward<Args>(args)...);
+            p._mode = Heap;
+        }
+
+        return p;
+    }
+
+    Base*       get()       { return raw_ptr(); }
+    const Base* get() const { return raw_ptr(); }
+
+    Base&       operator*()       { return *raw_ptr(); }
+    const Base& operator*() const { return *raw_ptr(); }
+
+    Base*       operator->()       { return raw_ptr(); }
+    const Base* operator->() const { return raw_ptr(); }
+
+    explicit operator bool() const { return _mode != Unspecified; }
+
+    Mode mode() const { return _mode; }
+};
+
+
+
+#endif
---
+++ cppautograd/src/SmallVector.hpp
@@ -0,0 +1,310 @@
+#ifndef __SMALL_VECTOR_HPP__
+#define __SMALL_VECTOR_HPP__
+
+#include <cstddef>
+#include <cstring>
+#include <type_traits>
+#include <new>
+#include <functional>
+#include <iterator>
+
+/*
+Small Vector implementation; uses SBO on a trivially copyable T. Optimal choices of SBO_BYTES are powers of 2 minus one, so 23, 31, 47, etc. based on 
+how much space you want to alloc for SBO. Some convience choices are in the form of SVec{24, 32, 48}<T>
+*/
+template<typename T, size_t SBO_BYTES>
+class SmallVec {
+    static_assert(std::is_trivially_copyable_v<T>, "SmallVec requires trivially copyable T.");
+    static_assert(SBO_BYTES >= sizeof(T), "SBO_BYTES must be >= sizeof(T).");
+
+    struct HeapData {
+        T*     ptr;     
+        size_t size;    
+        size_t cap;     
+    };                  
+
+    static constexpr size_t SBO_CAP = SBO_BYTES / sizeof(T);
+
+    union Storage {
+        HeapData heap;
+
+        struct {
+            alignas(T) std::byte data[SBO_BYTES];
+            unsigned char size;       
+        } sbo;
+
+        Storage() {}
+        ~Storage() {}
+    } storage;
+
+    bool using_sbo() const noexcept {
+        return storage.sbo.size <= SBO_CAP;
+    }
+
+    bool using_heap() const noexcept {
+        return storage.sbo.size > SBO_CAP;
+    }
+
+    void migrate_to_heap() {
+        const T* arr = reinterpret_cast<const T*>(storage.sbo.data);
+
+        size_t sz  = SBO_CAP;
+        size_t cap = (SBO_CAP < 4 ? 4 : SBO_CAP * 2);
+
+        T* newptr = static_cast<T*>(::operator new(cap * sizeof(T)));
+        std::memcpy(newptr, arr, sz * sizeof(T));
+
+        storage.heap.ptr  = newptr;
+        storage.heap.size = sz;
+        storage.heap.cap  = cap;
+
+        storage.sbo.size = SBO_CAP + 1;
+    }
+
+    void grow_heap() {
+        size_t newcap = storage.heap.cap * 2;
+        T* newptr = static_cast<T*>(::operator new(newcap * sizeof(T)));
+
+        std::memcpy(newptr, storage.heap.ptr, storage.heap.size * sizeof(T));
+
+        ::operator delete(storage.heap.ptr);
+        storage.heap.ptr = newptr;
+        storage.heap.cap = newcap;
+    }
+
+public:
+
+    SmallVec() noexcept {
+        storage.sbo.size = 0;
+    }
+
+    SmallVec(std::initializer_list<T> init) {
+        size_t n = init.size();
+
+        if (n <= SBO_CAP) {
+            storage.sbo.size = static_cast<unsigned char>(n);
+            std::memcpy(storage.sbo.data, init.begin(), n * sizeof(T));
+            return;
+        }
+
+        size_t cap = (n < 4 ? 4 : n);
+        T* newptr = static_cast<T*>(::operator new(cap * sizeof(T)));
+
+        std::memcpy(newptr, init.begin(), n * sizeof(T));
+
+        storage.heap.ptr  = newptr;
+        storage.heap.size = n;
+        storage.heap.cap  = cap;
+
+        storage.sbo.size = SBO_CAP + 1;
+    }
+
+    template <std::forward_iterator It>
+    SmallVec( It from, It to ) {
+        storage.sbo.size = 0;
+
+        while ( from != to ) {
+            emplace_back( *from );
+            ++from;
+        }
+    }
+
+
+    ~SmallVec() {
+        if (using_heap()) ::operator delete(storage.heap.ptr);
+    }
+
+    SmallVec(const SmallVec& other) noexcept {
+        if (other.using_sbo()) {
+            storage = other.storage;
+            return;
+        }
+
+        size_t sz  = other.storage.heap.size;
+        size_t cap = other.storage.heap.cap;
+        T* newptr = static_cast<T*>(::operator new(cap * sizeof(T)));
+
+        std::memcpy(newptr, other.storage.heap.ptr, sz * sizeof(T));
+        storage.heap.ptr  = newptr;
+        storage.heap.size = sz;
+        storage.heap.cap  = cap;
+
+        storage.sbo.size = SBO_CAP + 1;
+        
+    }
+
+    SmallVec& operator=(const SmallVec& other) noexcept {
+        if (this == &other) return *this;
+        this->~SmallVec();
+        new (this) SmallVec(other);
+        return *this;
+    }
+
+    SmallVec(SmallVec&& other) noexcept {
+        if (other.using_sbo()) {
+            storage = other.storage;
+            return;
+        }
+
+        storage.heap = other.storage.heap;
+        storage.sbo.size = SBO_CAP + 1;
+
+        other.storage.sbo.size = 0;
+    
+    }
+
+    SmallVec& operator=(SmallVec&& other) noexcept {
+        if (this == &other) return *this;
+        this->~SmallVec();
+        new (this) SmallVec(std::move(other));
+        return *this;
+    }
+
+    size_t size() const noexcept {
+        return using_sbo() ? storage.sbo.size : storage.heap.size;
+    }
+
+    bool empty() const noexcept {
+        return size() == 0;
+    }
+
+    const T& operator[](size_t i) const noexcept {
+        return using_sbo() ? reinterpret_cast<const T*>(storage.sbo.data)[i] : storage.heap.ptr[i];
+    }
+
+    T& operator[](size_t i) noexcept {
+        return using_sbo() ? reinterpret_cast<T*>(storage.sbo.data)[i] : storage.heap.ptr[i];
+    }
+
+    const T* data() const noexcept {
+        return using_sbo() ? reinterpret_cast<const T*>(storage.sbo.data) : storage.heap.ptr;
+    }
+
+    T* data() noexcept {
+        return using_sbo() ? reinterpret_cast<T*>(storage.sbo.data) : storage.heap.ptr;
+    }
+
+    void push_back(const T& v) {
+        if (using_sbo()) {
+            if (storage.sbo.size < SBO_CAP) {
+                reinterpret_cast<T*>(storage.sbo.data)[storage.sbo.size++] = v;
+                return;
+            }
+            migrate_to_heap();
+        }
+
+        if (storage.heap.size == storage.heap.cap) grow_heap();
+
+        storage.heap.ptr[storage.heap.size++] = v;
+    }
+
+    void reserve(size_t new_cap) {
+        if (using_sbo()) {
+            if (new_cap <= SBO_CAP) return;
+
+            size_t old_size = storage.sbo.size;
+            const T* old_data = reinterpret_cast<const T*>(storage.sbo.data);
+
+            std::size_t heap_cap = (new_cap < 4 ? 4 : new_cap);
+            T* newptr = static_cast<T*>(::operator new(heap_cap * sizeof(T)));
+
+            std::memcpy(newptr, old_data, old_size * sizeof(T));
+
+            storage.heap.ptr  = newptr;
+            storage.heap.size = old_size;
+            storage.heap.cap  = heap_cap;
+
+            storage.sbo.size = SBO_CAP + 1;
+            return;
+        }
+
+        if (new_cap <= storage.heap.cap) return;
+
+        size_t old_cap  = storage.heap.cap;
+        size_t old_size = storage.heap.size;
+
+        size_t heap_cap = new_cap;
+
+        if (heap_cap < old_cap * 2) heap_cap = old_cap * 2;
+
+        T* newptr = static_cast<T*>(::operator new(heap_cap * sizeof(T)));
+        std::memcpy(newptr, storage.heap.ptr, old_size * sizeof(T));
+
+        ::operator delete(storage.heap.ptr);
+        storage.heap.ptr = newptr;
+        storage.heap.cap = heap_cap;
+    }
+
+
+    void emplace_back(const T& v) {
+        push_back(v);
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args) {
+        static_assert(std::is_trivially_copy_constructible_v<T>, "SmallVec<T>::emplace_back requires trivial T.");
+        T temp{ std::forward<Args>(args)... };
+        push_back(temp);
+    }
+
+    T& front() noexcept {
+        return (*this)[0];
+    }
+
+    const T& front() const noexcept {
+        return (*this)[0];
+    }
+
+    T& back() noexcept {
+        return (*this)[size() - 1];
+    }
+
+    const T& back() const noexcept {
+        return (*this)[size() - 1];
+    }
+
+    using iterator = T*;
+    using const_iterator = const T*;
+
+    iterator begin() noexcept {
+        return data();
+    }
+
+    iterator end() noexcept {
+        return data() + size();
+    }
+
+    const_iterator begin() const noexcept {
+        return data();
+    }
+
+    const_iterator end() const noexcept {
+        return data() + size();
+    }
+
+    const_iterator cbegin() const noexcept {
+        return data();
+    }
+
+    const_iterator cend() const noexcept {
+        return data() + size();
+    }
+
+};
+
+template <class T>
+using SVec24 = SmallVec<T, 23>;
+
+template <class T>
+using SVec32 = SmallVec<T, 31>;
+
+template <class T>
+using SVec48 = SmallVec<T, 47>;
+
+template <class T>
+using RefVec24 = SVec24< std::reference_wrapper< T >>;
+
+template <class T>
+using RefVec32 = SVec32< std::reference_wrapper< T >>;
+
+#endif
